e01d81900afb9283074d6d468020e9226585899d
alikk <161784396+alikk20@users.noreply.github.com>
2024-05-03 09:14:33 +0700

    Update

--- proses_login.php
+++ proses_login.php
@@ -1,20 +1,26 @@
 <?php
 session_start();
 include 'config/connect.php';
-
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$query9 = mysqli_query($is_connect, "SELECT * FROM client WHERE username='$username' AND password='$password'");
-$data = mysqli_fetch_assoc($query9);
+
+$querry =mysqli_query($is_connect, "SELECT * FROM siswa WHERE username='$username'");
+// $result = mysqli_query($is_connect, $querry);
+
+
+$data = mysqli_fetch_assoc($querry);
+
 
 if(NULL != $data){
-
-  $_SESSION['id'] = $data['id'];
-  $_SESSION['username'] = $username;
-  $_SESSION['password'] = $password;
-
-  header('Location: index.php');
-} else {
-  header('Location: index.php'); 
+   
+    $_SESSION['id'] = $data['id'];
+    $_SESSION['username'] = $username;
+   
+    header('Location: index.php');
+    $query1 = mysqli_query($is_connect, "UPDATE siswa SET last_log = now() where id =". $_SESSION['id']);
+    $data1 = mysqli_fetch_assoc($query1);
+}else{
+    echo 'Ahhh Ahhhh';
+    echo "<a href='authentication-login.php'>Kembali</a>";
 }
